de5f8ec3d0d70828fe2ea1116b5ce6584e25e8a1
Arthur STM <asone7776@gmail.com>
2019-03-20 15:22:53 +0500

    Company last changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route ,Switch} from 'react-router-dom';
 import App from "../components/app";
 import Hrm from "../containers/hrm/index"
-import Home from '../components/Company/Info';
+import AdminPanel from '../containers/AdminPanel/AdminPanel';
 const PublicRoute = ()=>{
     return(
         <div>
@@ -16,10 +16,10 @@ const PublicRoute = ()=>{
                         path="/hrm"
                         component={Hrm}
                     />
-                    {/*<Route*/}
-                      {/*path="/home"*/}
-                      {/*component={Home}*/}
-                    {/*/>*/}
+                    <Route
+                      path="/admin-panel"
+                      component={AdminPanel}
+                    />
                 </Switch>
             </BrowserRouter>
         </div>
